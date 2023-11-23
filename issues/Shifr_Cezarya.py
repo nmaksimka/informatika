b6a1cfe7a36371cd5b1ed892d741770b10679c22
nmaksimka <maksimnikonov407@gmail.com>
2023-11-23 20:05:37 +0600

    i fix Shifr_Cezarya.py

--- issues/Shifr_Cezarya.py
+++ issues/Shifr_Cezarya.py
@@ -1,37 +1,34 @@
-def caesar_decipher(word):
+def caesar_cipher(text, shift):
     result = ""
-    for char in word:
+    for char in text:
         if char.isalpha():
-            shift = ord(char) - ord('a') if char.islower() else ord(char) - ord('A')
-            new_char = chr((ord(char) + shift) % 26 + ord('a')) if char.islower() else chr((ord(char) + shift) % 26 + ord('A'))
-            result += new_char
+            start = ord('a') if char.islower() else ord('A')
+            result += chr((ord(char) - start + shift) % 26 + start)
         else:
             result += char
     return result
 
-def caesar_cipher(word):
+def caesar_decipher(text, shift):
     result = ""
-    for char in word:
+    for char in text:
         if char.isalpha():
-            shift = ord(char) - ord('a') if char.islower() else ord(char) - ord('A')
-            new_char = chr((ord(char) - shift) % 26 + ord('a')) if char.islower() else chr((ord(char) - shift) % 26 + ord('A'))
-            result += new_char
+            start = ord('a') if char.islower() else ord('A')
+            result += chr((ord(char) - start - shift) % 26 + start)
         else:
             result += char
     return result
-def decrypt_text(encrypt_text):
-    words = encrypt_text.split()
-    decrypted_words = [caesar_decipher(word) for word in words]
-    decrypted_text = ' '.join(decrypted_words)
-    return decrypted_text
 
-def encrypted_text(descr):
-    words = descr.split()
-    encrypted_words = [caesar_cipher(word) for word in words]
-    encrypt_text = ''.join(encrypted_words)
-    return encrypt_text
+def main():
+    text = input("Введите текст для шифрования: ")
+    shift = int(input("Введите сдвиг для шифра Цезаря: "))
 
-input_text = input("Введите строку текста на английском языке: ")
+    # Шифрование
+    encrypted_text = caesar_cipher(text, shift)
+    print(f"Зашифрованный текст: {encrypted_text}")
 
-print("Зашифрованный текст:", decrypt_text(input_text))
-print("Расшифрованный текст:", decrypt_text(encrypted_text(input_text)))
+    # Дешифрование
+    decrypted_text = caesar_decipher(encrypted_text, shift)
+    print(f"Расшифрованный текст: {decrypted_text}")
+
+if __name__ == "__main__":
+    main()
