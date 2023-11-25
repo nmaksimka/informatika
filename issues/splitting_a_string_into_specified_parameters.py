3af3f4b57bf6462d4d15fab605558ea5687a0c11
nmaksimka <maksimnikonov407@gmail.com>
2023-11-25 12:30:51 +0600

    i fix splitting_a_string_into_specified_parameters.py

--- issues/splitting_a_string_into_specified_parameters.py
+++ issues/splitting_a_string_into_specified_parameters.py
@@ -7,33 +7,24 @@
 #: 1,5,2,0,0,5 Всего: 6
 #Символы: !,!,,,.,? Всего: 5
 #Пробелов всего: 12
-
+import re
 tests = [
     "Привет! Меня зовут Влад! Мне 15, я родился в 2005 году. А ты?"
 ]
 
 for k in tests:
-    counter_b = 0
-    counter_d = 0
-    counter_s = 0
-    counter_p = 0
-    l_b = []
-    l_d = []
-    l_s = []
-    for i in k:
-        if i.lower() in '[а-яё]+|[a-z]+':  # or i.lower() in ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'ё']:
-            l_b.append(i + ',')
-            counter_b += 1
-        elif i in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
-            l_d.append(i + ',')
-            counter_d += 1
-        elif i in ['!', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']:
-            l_s.append(i + ',')
-            counter_s += 1
-        elif i == ' ':
-            counter_p += 1
+    # Поиск букв
+    letters = re.findall(r'[а-яёА-ЯЁ]', k)
+    print('Буквы:', *letters, 'Всего:', len(letters))
 
-    print('Буквы:', *l_b, 'Всего:', counter_b)
-    print('Цифры:', *l_d, 'Всего:', counter_d)
-    print('Символы:', *l_s, 'Всего:', counter_s)
-    print('Пробелов всего:', counter_p)
+    # Поиск цифр
+    digits = re.findall(r'\d', k)
+    print('Цифры:', *digits, 'Всего:', len(digits))
+
+    # Поиск символов
+    symbols = re.findall(r'[^\w\s]', k)
+    print('Символы:', *symbols, 'Всего:', len(symbols))
+
+    # Поиск пробелов
+    spaces = re.findall(r'\s', k)
+    print('Пробелов всего:', len(spaces))
